cd847f4c7b57992c82ec475aa18962024234c138
sevro49 <21120205303@ismu.edu.tr>
2023-03-19 22:55:02 +0300

    operators

---
+++ 4_operators/script.js
@@ -0,0 +1,29 @@
+let result;
+let a = 10,
+    b = 20,
+    c = 30;
+
+// result = a + b;
+// result = a - b;
+// result = a * b;
+// result = a / b;
+// result = a % b;
+// result = a++; // a + 1
+// result = ++a; // a + 1
+
+// result = b;
+// result += a;
+// result *= b;
+// result -= c;
+// result /= c;
+// result %= c;
+
+// result = (a == b);
+// result = (a != b);
+// result = (3 === "3");
+
+
+
+
+
+console.log(result);
