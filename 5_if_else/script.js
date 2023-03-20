9f5753d4b8c1314b599694869d4d4accbeab8aba
sevro49 <21120205303@ismu.edu.tr>
2023-03-20 21:17:05 +0300

    if else

---
+++ 5_if_else/script.js
@@ -0,0 +1,22 @@
+// let username = "sevro49";
+// let password = 12345;
+
+// if (username == "sevro49") {
+//     if (password == 12345) {
+//         console.log("Logged in app\nHello, Welcome");
+//     } else console.log("Wrong password");
+// } else {
+//     console.log("Wrong username");
+// }
+
+// age >= 18
+// graduation == "high school" or graduation == "bachelors degree"
+
+let age = 20;
+let graduation = "bachelors degree";
+
+if (age >= 18 && (graduation == "bachelors degree" || graduation == "high school")) {
+    console.log("can get driver's license");
+} else {
+    console.log("Can not get license");
+}
